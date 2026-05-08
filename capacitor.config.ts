b749920665d5d045b7486db7c13a3c6b26707aeb
R.N <rabi.nateghi@gmail.com>
2026-05-08 11:35:18 +0330

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.gasino.app',
-  appName: 'گازینو',
+  appName: 'Gasino',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
